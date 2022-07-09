835f835cbfbbe3119c36866d854e134f90a5ca38
Kali <nope@example.com>
2022-07-09 16:43:25 +0100

    Added basic connectivity.

--- Target/target_factory.py
+++ Target/target_factory.py
@@ -9,12 +9,12 @@ def create_target(target: str):
     if ":" in target:
         target = target.split(":")
         try:
-            return Target(remote(target[0], int(target[1])))
+            return Target(remote(target[0], int(target[1])), True)
         except PwnlibException:
             print(f'Unable to connect to {target[0]}:{target[1]}, please ensure host is up.\nFormat: IP:PORT')
     else:
         try:
-            return Target(process(target))
+            return Target(process(target.split(" ")), False)
         except PwnlibException:
             print(f'Unable to start local process {target}, please ensure the binary name is correct.\nExample: '
                   f'./my_vuln_binary')
