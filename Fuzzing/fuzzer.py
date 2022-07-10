496f928ea4e6fc050bbe875b929eef036696f841
Kali <nope@example.com>
2022-07-10 22:37:09 +0100

    Completed initial fuzzer for 32/64 bits

--- Fuzzing/fuzzer.py
+++ Fuzzing/fuzzer.py
@@ -1,9 +1,14 @@
+import string
+
 from Target.target import Target
 from Target.target_factory import create_target
 from pwn import *
 
 
 class Fuzzer:
+    registers_arch = {"64": ["rip", "rsp", "rbp"],
+                      "32": ["eip", "esp", "ebp"]}
+
     def indentify_crash(self, target: Target, attempts: int = 100):
         _size = 100
         for i in range(attempts):
@@ -21,11 +26,11 @@ class Fuzzer:
                 target = create_target(target.process)
         return 0
 
-    #TODO INCOMPLETE
     def get_offset(self, target: Target, _size: int):
+        pattern_style = string.ascii_uppercase
         if target.is_remote:
             return -1
-        pattern = cyclic(length=_size, alphabet=string.ascii_uppercase)
+        pattern = cyclic(length=_size, alphabet=pattern_style)
         gdb_execute = f'/usr/bin/gdb -pid={target.get_pid()}'
         debugger = create_target(gdb_execute)
 
@@ -35,12 +40,31 @@ class Fuzzer:
             debugger.receive_data()
 
         target.receive_data()
+        debugger.send_data("set style enabled off")
+        debugger.receive_data()
+        debugger.send_data("continue")
+        debugger.receive_data()
         target.send_data(pattern)
+        debugger.receive_data()
 
-        debugger.send_data("info registers\n")
-        print(debugger.receive_data())
+        for register in self.registers_arch[str(target.get_bit_arch())]:
+            debugger.send_data(f"x ${register}")
+            register = debugger.receive_data().replace("\t", " ").replace("\n", " ").split(" ")
+            pointer_register = register[0]
 
-        # python print(gdb.selected_frame().read_register('rip'))
+            if pointer_register[len(pointer_register)-1] == ":":
+                pointer_register = pointer_register[:-1]
 
+            if len(pointer_register) > 10:
+                pointer_register = pointer_register[2:]
+                while len(pointer_register) > 8:
+                    pointer_register = pointer_register[1:]
+                pointer_register = f'0x{pointer_register}'
 
+            offset = cyclic_find(int(pointer_register, 16), alphabet=pattern_style)
+            if offset > 0:
+                debugger.connection.kill()
+                return offset
 
+        debugger.connection.kill()
+        return -1
