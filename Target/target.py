a979932e2f6db3484788938a096534cae73e0d71
Kali <nope@example.com>
2022-07-10 12:20:11 +0100

    Added initial crash identifer.

--- Target/target.py
+++ Target/target.py
@@ -1,16 +1,26 @@
 from pwn import *
 
-class Target:
 
-    def __init__(self, connection, remote: bool):
+class Target:
+    def __init__(self, connection, remote: bool, process):
         self.connection = connection
         self.remote = remote
+        self.process = process
 
     def receive_data(self):
         data = ""
         while True:
-            response = self.connection.recv(1024, 10).decode()
+            try:
+                response = self.connection.recv(1024, 1).decode()
+            except EOFError:
+                break
             if len(response) < 1:
                 break
             data += response
         return data
+
+    def send_data(self, data: str):
+        self.connection.sendline(data.encode())
+
+    def is_alive(self):
+        return True if self.connection.poll() is None else False
--- Target/target_factory.py
+++ Target/target_factory.py
@@ -9,12 +9,13 @@ def create_target(target: str):
     if ":" in target:
         target = target.split(":")
         try:
-            return Target(remote(target[0], int(target[1])), True)
+            return Target(remote(target[0], int(target[1])), True, target)
         except PwnlibException:
             print(f'Unable to connect to {target[0]}:{target[1]}, please ensure host is up.\nFormat: IP:PORT')
     else:
         try:
-            return Target(process(target.split(" ")), False)
+            return Target(process(target.split(" ")), False, target)
         except PwnlibException:
             print(f'Unable to start local process {target}, please ensure the binary name is correct.\nExample: '
-                  f'./my_vuln_binary')
+                  f'./my_vuln_binary'
+                  f'\nIt\'s recommended to use the full path!')
